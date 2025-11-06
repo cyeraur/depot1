20e12cc6d6c21a8e300de45c15daf08a7244696f
ActionReplayy <rom1sirantoine@gmail.com>
2025-11-06 16:30:45 +0100

    Update index.php

--- index.php
+++ index.php
@@ -1,6 +1,6 @@
 <?php
      echo 'mon premier test sur git' ;
      echo '<table border=1>' :
-     echo '<tr><td>titre</td><td>description</td></tr>' ;
+     echo '<tr><td>titre</td><td>description CYRIAN</td></tr>' ;
      echo '</table>' ;
 ?>
